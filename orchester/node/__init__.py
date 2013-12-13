bfe97abf7fe0838452a2d9406ad46f37d8c4d996
Matthieu Rosinski <korrigan@staff.42.fr>
2013-12-13 08:51:06 +0100

    extending node /

--- orchester/node/__init__.py
+++ orchester/node/__init__.py
@@ -9,17 +9,22 @@ from flask import jsonify
 version = '0.0.1'
 api_version = '0.0.1'
 
-app = Flask(__name__)
+app = Flask('orchester.node')
 
 
 @app.route('/')
 def index():
     """
-    The API root
-    Provides nothing useful unless version reports.
+    The API root endpoint.
+    Provides information about the node and its capacities.
 
     """
     data = {
+        'service': 'node',
+        'capabilities': [
+            'instance',
+            'lb',
+        ],
         'version': version,
         'api_version': api_version,
     }
