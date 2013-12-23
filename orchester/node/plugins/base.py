f6c71473d6491a4058229fb3b377c68b477a00b5
Matthieu Rosinski <korrigan@staff.42.fr>
2013-12-23 17:12:24 +0100

    added WorkerAbstractBasePlugin class in orchester.node.plugins.base module

---
+++ orchester/node/plugins/base.py
@@ -0,0 +1,38 @@
+"""
+This module implements base plugins for orchester-node
+
+"""
+
+from orchester.plugins.base import get_class
+
+
+class WorkerAbstractBasePlugin(get_class()):
+    """
+    This class is the base class for worker node plugins
+    Every node plugins should inherits of this class
+
+    """
+
+    @classmethod
+    def get_extra_kwargs(kls, app):
+        """
+        This method returns the extra configuration kwargs needed to be sent to
+        the node for worker deployment.
+
+        It takes information from app wich is an instance of
+        orchester.models.application.Application
+
+        By default returns None, an useful implementation must returns a dict
+
+        Child class must implement all their required argument here
+
+        Node daemon will instanciate the plugin with these keywords
+        arguments if provided
+
+        """
+        return None
+
+
+def get_class():
+    """Returns the WorkerAbstractBasePlugin class"""
+    return WorkerAbstractBasePlugin
