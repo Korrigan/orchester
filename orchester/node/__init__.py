6c5dfbafc124a0382374697c03bdbe6467f6cbf9
Matthieu Rosinski <korrigan@staff.42.fr>
2013-12-23 14:42:34 +0100

    node using api helper functions

--- orchester/node/__init__.py
+++ orchester/node/__init__.py
@@ -4,29 +4,9 @@ This module is the entry point of the orchester-node daemon
 """
 
 from flask import Flask
-from flask import jsonify
 
-version = '0.0.1'
-api_version = '0.0.1'
+from .api import index
+
 
 app = Flask('orchester.node')
-
-
-@app.route('/')
-def index():
-    """
-    The API root endpoint.
-    Provides information about the node and its capacities.
-
-    """
-    data = {
-        'service': 'node',
-        'capabilities': [
-            'instance',
-            'lb',
-        ],
-        'version': version,
-        'api_version': api_version,
-    }
-    return jsonify(data)
- 
+app.register_blueprint(index)
