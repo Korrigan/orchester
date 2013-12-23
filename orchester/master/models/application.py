7b37d8980c9337b86d92e27e564d3e316c999f07
Matthieu Rosinski <korrigan@staff.42.fr>
2013-12-23 16:58:51 +0100

    added base for master models

---
+++ orchester/master/models/application.py
@@ -0,0 +1,41 @@
+"""
+This module contain application related models
+
+"""
+
+from orchester.master import db
+
+from .worker import Worker
+
+
+class Application(db.Document):
+    """
+    This class represents an application
+
+    """
+    domain_name = db.StringField(unique=True)
+    code_type = db.StringField()
+    code_url = db.StringField()
+    code_tag = db.StringField()
+    min_workers = db.IntField()
+    max_workers = db.IntField()
+    env_vars = db.DictField()
+    workers = db.ListField(db.EmbeddedDocumentField(Worker))
+
+    @property
+    def private_key(self):
+        """Read and returns the private key of the app"""
+        return "toto%d" % self.id
+
+    @property
+    def public_key(self):
+        """Read and returns the private key of the app"""
+        return "tata%d" % self.id
+
+    def gen_rsa_keypair(self):
+        """
+        This method generate a unique keypair for the application
+
+        """
+        pass
+
---
+++ orchester/master/models/node.py
@@ -0,0 +1,36 @@
+"""
+This module handles node related models
+
+"""
+
+from orchester.plugins import get_node_plugin_class
+from orchester.master import db
+
+
+class Node(db.Document):
+    """
+    This class represents a node instance
+
+    """
+    host = db.StringField()
+
+    def deploy(self, app):
+        """
+        This method deploys the instance of Application to the node
+
+        """
+        plugin = 'docker'
+        klass = get_node_plugin_class(plugin)
+        data = {
+            'plugin_name': plugin,
+            'private_key': app.private_key,
+            'code_url': app.code_url,
+            'env_vars': app.env_vars,
+            'extra': klass.get_extra_kwargs(app)
+            }
+        if app.code_tag:
+            data['code_tag'] = app.code_tag
+        ret = "" # From node API call
+        ret_data = json.decode(ret)
+        app.workers.push(Worker(host=self, worker_id=ret_data['id']))
+
