814bcd710ba56e914a2b806467f571343dc44e6e
Matthieu Rosinski <korrigan@staff.42.fr>
2013-12-25 20:14:31 +0100

    few bugfixes in generic views

--- orchester/api/views.py
+++ orchester/api/views.py
@@ -3,7 +3,8 @@ Misc utils views for orchester API
 
 """
 
-from flask import jsonify
+from flask import json
+from flask import abort, url_for, jsonify
 from flask.views import MethodView
 
 from orchester.api import api_version
@@ -22,6 +23,45 @@ def index(service, version, capabilities=[], api_version=api_version):
         'api_version': api_version,
     }
     return jsonify(data)
+
+
+def get_field_keys(k):
+    """
+    This function takes either a tuple or a string as parameter and returns
+    a tuple of two strings.
+
+    The first arg of the tuple is either the string or the first arg of the
+    tuple.
+    The second arg is the string or the first arg if k is a string or if the
+    2nd arg of k is None; else it's the string or the first arg of the tuple.
+
+    This function is here to deal with fields list in classes below.
+
+    """
+    key = None
+    obj_key = None
+    if isinstance(k, tuple):
+        (key, obj_key) = k
+        if not obj_key:
+            obj_key = key
+    else:
+        key = k
+        obj_key = k
+    return (key, obj_key)
+
+
+def check_field_key(k, list):
+    """
+    This function check if the key is in the field list
+
+    This function is here to deal with fields list in classes below
+
+    """
+    for cmp in list:
+        (key, obj_key) = get_field_keys(cmp)
+        if key == k:
+            return (key, obj_key)
+    return (None, None)
 
 
 class APIListCreateView(MethodView):
@@ -67,8 +107,37 @@ class APIListCreateView(MethodView):
         }
         for obj in qs:
             data[self.data_list_key].append(url_for(self.view_name,
-                                                    **{self.view_arg_name: obj._id}))
+                                                    **{self.view_arg_name: obj.id}))
         return jsonify(data)
+
+    def post(self):
+        """
+        This method creates an object from the post data
+        If successfull redirects to the detail handler for the newly
+        created object.
+
+        """
+        from flask import request, redirect
+
+        data = json.loads(request.data)
+        kw = {}
+        allowed_fields = self.required_fields + self.optional_fields
+        for (k, v) in data.iteritems():
+            print k in allowed_fields
+            (key, obj_key) = check_field_key(k, allowed_fields)
+            if not key:
+                print "invaild field %s" % k
+                abort(400) # to fix
+            kw[obj_key] = v
+        for k in self.required_fields:
+            (key, obj_key) = get_field_keys(k)
+            if not obj_key in kw:
+                print "missing field %s" % k
+                abort(400)
+        obj = self.model(**kw)
+        obj.save()
+        return redirect(url_for(self.view_name,
+                                **{self.view_arg_name: obj.id}))
 
 
 class APIDetailView(MethodView):
@@ -96,9 +165,9 @@ class APIDetailView(MethodView):
 
     def get_object(self, obj_id):
         """Returns the model object filtering by kwargs"""
-        if not model:
+        if not self.model:
             abort(404)
-        return self.model.objects.get_or_404(_id=obj_id)
+        return self.model.objects.get_or_404(id=obj_id)
 
     def get_object_data(self, obj):
         """
@@ -106,21 +175,12 @@ class APIDetailView(MethodView):
         This method looks for a display_fields attribute
 
         """
-        def _get_keys(k):
-            """Returns the correct keys for object and data"""
-            if isinstance(f, tuple):
-                (key, obj_key) = f
-                if not obj_key:
-                    obj_key = key
-            else:
-                key = f
-                obj_key = f
-            return (key, objkey)
-
         data = {}
         for f in self.display_fields:
-            (k, obj_k) = _get_keys(f)
-            data[k] = getattr(obj, obj_k)
+            (k, obj_k) = get_field_keys(f)
+            v = getattr(obj, obj_k)
+            if v:
+                data[k] = v
         return data
 
     def get_object_url(self, obj):
@@ -129,23 +189,25 @@ class APIDetailView(MethodView):
 
         """
         from flask import url_for
-        return url_for(self.view_name, **{self.view_arg_name: obj._id})
+        return url_for(self.view_name, **{self.view_arg_name: obj.id})
 
-    def get(self, obj_id):
+    def get(self, **kwargs):
         """
         This method is the GET endpoint
         It returns the data from object and adds a url field
 
         """
+        obj_id = kwargs[self.view_arg_name]
         obj = self.get_object(obj_id)
         data = self.get_object_data(obj)
         data['url'] = self.get_object_url(obj)
         return jsonify(data)
 
-    def delete(self, obj_id):
+    def delete(self, **kwargs):
         """
         This method is the DELETE endpoint for the object
 
         """
+        obj_id = kwargs[self.view_arg_name]
         obj = self.get_object(obj_id)
         obj.delete()
