bc9e63b6fa1089a1cd3d8f993c951a9396cae7b2
Matthieu Rosinski <korrigan@staff.42.fr>
2013-12-27 05:49:45 +0100

    few more tests for the /app master API endpoint

--- orchester/tests/master/api.py
+++ orchester/tests/master/api.py
@@ -16,9 +16,17 @@ class MasterAPITestCase(unittest.TestCase):
     Setup the tests context
 
     """
+    database = 'orchester_test'
 
     def setUp(self):
+        master.setup(db_name=self.database)
         self.app = master.app.test_client()
+    
+    def tearDown(self):
+        from pymongo import MongoClient
+
+        c = MongoClient()
+        c.drop_database(self.database)
 
 
 class IndexTestCase(MasterAPITestCase):
@@ -43,3 +51,83 @@ class IndexTestCase(MasterAPITestCase):
         assert data.has_key('version')
         assert data.has_key('api_version')
 
+
+class AppTestMixin(object):
+    """
+    Base class providing utilities for app testing
+
+    """
+
+    def create_app(self, **kwargs):
+        """Creates an application and returns the post return"""
+        req = {'domain_name': 'example.com',
+               'code_type': 'python',
+               'code_tag': 'test',
+               'code_url': 'https://example.com/repo.git',
+               'min_workers': 0,
+               'max_workers': 0,
+           }
+        for (k, v) in kwargs.iteritems():
+            req[k] = v
+        with master.app.test_request_context():
+            return self.app.post('/app/', content_type='application/json', data=json.dumps(req))
+
+
+class AppCreateTestCase(MasterAPITestCase, AppTestMixin):
+    """
+    Test the /app master API endpoint for app creation
+
+    """
+
+    def test_app_create(self):
+        rv = self.create_app()
+        assert rv.status_code == 302 or rv.status_code == 301
+        json.loads(rv.data)
+
+class AppListTestCase(MasterAPITestCase, AppTestMixin):
+    """
+    Test the listing feature of the /app/ endpoint
+    
+    """
+
+    def test_app_list(self):
+        self.create_app()
+        rv = self.app.get('/app/')
+        assert rv.status_code == 200
+        data = json.loads(rv.data)
+        assert data.has_key('count')
+        assert data.has_key('apps')
+        assert data['count'] != 0
+        assert len(data['apps']) == data['count']
+
+
+class AppDetailTesstCase(MasterAPITestCase, AppTestMixin):
+    """
+    Tests the detail app API endpoint /app/<app_id>
+
+    """
+    def test_app_detail(self):
+        rv = self.create_app()
+        data = json.loads(rv.data)
+        rv = self.app.get(data['url'])
+        data = json.loads(rv.data)
+        assert data.has_key('id')
+        assert data.has_key('domain_name')
+        assert data.has_key('min_workers')
+        assert data.has_key('max_workers')
+        assert data.has_key('code_type')
+        assert data.has_key('code_url')
+        assert data.has_key('code_tag')
+        assert data.has_key('public_key')
+        assert data.has_key('env_vars')
+
+    def test_app_update(self):
+        pass
+
+    def test_app_delete(self):
+        rv = self.create_app()
+        data = json.loads(rv.data)
+        rv = self.app.delete(data['url'])
+        json.loads(rv.data)
+        rv = self.app.get(data['url'])
+        assert rv.status_code == 404
