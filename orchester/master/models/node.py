20481aa1e4ed6285bc461a479cf482af6b775c60
Matthieu Rosinski <korrigan@staff.42.fr>
2014-01-28 12:33:09 +0100

    fixed deployment according to reworked Worker model

--- orchester/master/models/node.py
+++ orchester/master/models/node.py
@@ -6,8 +6,10 @@ This module handles node related models
 from orchester.plugins import get_node_plugin_class
 from orchester.master import db
 
+from . import DisplayableModelMixin
 
-class Node(db.Document):
+
+class Node(db.Document, DisplayableModelMixin):
     """
     This class represents a node instance
 
@@ -19,7 +21,7 @@ class Node(db.Document):
         This method deploys the instance of Application to the node
 
         """
-        plugin = 'docker'
+        plugin = 'dummy' # We should have a way to add a plugin selector
         klass = get_node_plugin_class(plugin)
         if not klass:
             raise "toto"
@@ -32,6 +34,7 @@ class Node(db.Document):
             }
         if app.code_tag:
             data['code_tag'] = app.code_tag 
-`       ret = '{id: "tachatte"}' # From node API call
+        ret = '{ "id": "tachatte" }' # From node API call
         ret_data = json.decode(ret)
-        app.workers.push(Worker(host=self, worker_id=ret_data['id']))
+        wkr = Worker(host=self, worker_id=ret_data['id'], app=app)
+        wkr.save()
